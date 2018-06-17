1b6cf50b51ad9486b4bd7c3a89104a3aae8fddfc
Humad <humad@live.com>
2018-06-17 02:00:51 -0700

    updated react example

--- React/ReactSnippets/camper.js
+++ React/ReactSnippets/camper.js
@@ -1,30 +1,53 @@
 class CampSite extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-      return (
-        <div>
-          <Camper/>
-        </div>
-      );
-    }
-  };
-  // change code below this line
-  
-  class Camper extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-  
-    render() {
-      return (
-        <div>
-          <p>{this.props.name}</p>
-        </div>
-      )
-    }
+  constructor(props) {
+    super(props);
   }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
   
-  Camper.defaultProps = {name: 'CamperBot'};
-  Camper.propTypes = {name: PropTypes.string.isRequired}
+class Camper extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.name}</p>
+      </div>
+    )
+  }
+}
+
+Camper.defaultProps = {name: 'CamperBot'};
+Camper.propTypes = {name: PropTypes.string.isRequired}
+
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      name: "React Rocks!"
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
