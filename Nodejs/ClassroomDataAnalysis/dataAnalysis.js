8d7e0c4be979c02f4b10c692450d84320c112108
Humad <humad@live.com>
2018-06-21 22:26:08 -0700

    updated classroomdataanalysis

--- Nodejs/ClassroomDataAnalysis/dataAnalysis.js
+++ Nodejs/ClassroomDataAnalysis/dataAnalysis.js
@@ -6,12 +6,16 @@
 // - wait_time
 // - help_time
 
-// SQL Query to get data for today: SELECT * FROM events WHERE ts >= '2018-06-13' AND ts < '2018-06-14' ORDER BY ts ASC;
+// SQL Query to get data for today: SELECT * FROM events WHERE ts >= '2018-06-19 16:00:00' AND ts <= '2018-06-20 05:00:00' ORDER BY ts ASC;
 
 const fs = require('fs');
 const refactoredData = [];
 const studentMap = {};
 const taMap = {};
+const realNames = JSON.parse(JSON.stringify(require('./raw_data/realNames.js')));
+const moment = require('moment-timezone');
+
+console.log(moment("2018-06-21T23:58:26.225Z").tz("America/Los_Angeles").format());
 
 // Adds basic student data to the refactored data array
 function addStudent(eventData) {
@@ -36,7 +40,7 @@ function addTA(eventData) {
             // Update wait time
             var waitTime = Date.parse(currentData.timeReceivedHelp) - Date.parse(currentData.timeEnteredQueue);
             var waitTimeInMinutes = waitTime / 60000;
-            waitTimeInMinutes = waitTimeInMinutes > 40 ? 40 : waitTimeInMinutes;
+            waitTimeInMinutes = waitTimeInMinutes > 60 ? 60 : waitTimeInMinutes;
             currentData.waitTime = Math.floor(waitTimeInMinutes * 100) / 100;
 
             break;
@@ -60,7 +64,6 @@ function cancelStudent(eventData) {
 
 // Updates information in refactored data array by updating student help time
 function completeHelp(eventData) {
-
     // Find data ("row") for assigned TA
     for (var i = refactoredData.length - 1; i >= 0; i--) {
         var currentData = refactoredData[i];
@@ -71,7 +74,7 @@ function completeHelp(eventData) {
             // Update help time
             var helpTime = Date.parse(currentData.timeCompleted) - Date.parse(currentData.timeReceivedHelp);
             var helpTimeInMinutes = helpTime / 60000;
-            helpTimeInMinutes = helpTimeInMinutes > 40 ? 40 : helpTimeInMinutes;
+            helpTimeInMinutes = helpTimeInMinutes > 60 ? 60 : helpTimeInMinutes;
             currentData.helpTime = Math.floor(helpTimeInMinutes * 100) / 100;
             break;
         }
@@ -174,6 +177,45 @@ function writeDataToFile() {
     fs.writeFile('./parsed_data/taData.json', JSON.stringify(taMap, null, 4), 'utf8', function(error, data){});
 }
 
+function getFormattedTime(dateTime) {
+    let momentDate = moment(dateTime).tz("America/Los_Angeles").format();
+    let date = momentDate.substring(0, 10);
+    let time = momentDate.substring(11, 16);
+    return date + " " + time;
+}
+
+function cleanData() {
+    for (var i = 0; i < refactoredData.length; i++) {
+        if (refactoredData[i].ta) {
+            refactoredData[i].ta = realNames[refactoredData[i].ta] ? realNames[refactoredData[i].ta] : refactoredData[i].ta;
+        }
+
+        if (refactoredData[i].student) {
+            refactoredData[i].student = realNames[refactoredData[i].student] ? realNames[refactoredData[i].student] : refactoredData[i].student;
+        }
+
+        if (refactoredData[i].timeCancelled) {
+            refactoredData[i].timeCancelled = getFormattedTime(refactoredData[i].timeCancelled);
+        }
+
+        if (refactoredData[i].timeCleared) {
+            refactoredData[i].timeCleared = getFormattedTime(refactoredData[i].timeCleared);
+        }
+
+        if (refactoredData[i].timeCompleted) {
+            refactoredData[i].timeCompleted = getFormattedTime(refactoredData[i].timeCompleted);
+        }
+
+        if (refactoredData[i].timeEnteredQueue) {
+            refactoredData[i].timeEnteredQueue = getFormattedTime(refactoredData[i].timeEnteredQueue);
+        }
+
+        if (refactoredData[i].timeReceivedHelp) {
+            refactoredData[i].timeReceivedHelp = getFormattedTime(refactoredData[i].timeReceivedHelp);
+        }
+    }
+}
+
 // Reads through JSON data and refactors it with relevant information
 function main(data) {
     for (var i = 0; i < data.length; i++) {
@@ -198,6 +240,8 @@ function main(data) {
                 removeStudent(data[i]);
         }
     }
+
+    cleanData();
 
     gatherStudentInformation();
     gatherTAInformation();
--- Nodejs/ClassroomDataAnalysis/index.js
+++ Nodejs/ClassroomDataAnalysis/index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require('path');
 const app = express();
+var moment = require('moment-timezone');
 
 app.set("port", (process.env.PORT || 8000));
 app.set('views', path.join(__dirname, 'views'));
@@ -21,30 +22,112 @@ setInterval(function() {
 
 // ---- //
 
-const analyzedData = require('./dataAnalysis')(require('./raw_data/every_day/6-18.json'));
+// Postgres setup
+const Pool = require('pg').Pool;
+
+var config = {
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    ssl: true
+}
+
+const pool = new Pool(config);
+
+
+// SELECT * FROM events WHERE ts >= '2018-06-19 16:00:00' AND ts <= '2018-06-20 05:00:00' ORDER BY ts ASC;
+// Time in Postgres is 7 hours ahead Pacific Time
+// 09:00 in Pacific Time is 16:00 in Postgres time
+// So for a normal Horizons day, we need data from (today, 16:00) to (tomorrow, 05:00)
+
+let analyzedData;
+
+function getDate(offset) {
+    return moment().add(offset, 'days').tz("America/Los_Angeles").format();
+}
+
+function updateData(callback) {
+    var todayString = getDate(0).substring(0, 10);
+    var tomorrowString = getDate(1).substring(0, 10);
+    let queryString = "SELECT * FROM events WHERE ts >= '" + todayString + " 16:00:00' AND ts <= '" + tomorrowString + " 05:00:00' ORDER BY ts ASC";
+
+    pool.query(queryString, function(err, result) {
+        if (err) {
+            callback(err);
+        } else {
+            jsonData = JSON.parse(JSON.stringify(result.rows));
+            analyzedData = require('./dataAnalysis')(jsonData);
+            callback(null);
+        }
+    });
+}
 
 app.get('/', function(req, res) {
-    res.render('index', {
-        today: '6/18',
-        data: analyzedData.refactoredData,
-        averageWaitTime: analyzedData.averageWaitTime,
-        averageHelpTime: analyzedData.averageHelpTime
+    var todayString = getDate(0).substring(0, 10);
+    updateData(function(err) {
+        if (err) {
+            res.render('index', {
+                today: todayString,
+                data: [],
+                averageWaitTime: -1,
+                averageHelpTime: -1
+            });
+        } else {
+            res.render('index', {
+                today: todayString,
+                data: analyzedData.refactoredData,
+                averageWaitTime: analyzedData.averageWaitTime,
+                averageHelpTime: analyzedData.averageHelpTime
+            });
+        }
     });
 });
 
 app.get('/student/:studentName', function(req, res) {
-    res.render('student', {
-        data: analyzedData.studentMap[req.params.studentName]
-    });
+    if (analyzedData) {
+        res.render('student', {
+            data: analyzedData.studentMap[req.params.studentName]
+        });
+    } else {
+        updateData(function(err) {
+            if (err) {
+                res.render('student', {
+                    data: null
+                });
+            } else {
+                res.render('student', {
+                    data: analyzedData.studentMap[req.params.studentName]
+                });
+            }
+        });
+    }
 });
 
 app.get('/ta/:taName', function(req, res) {
-    res.render('ta', {
-        data: analyzedData.taMap[req.params.taName]
-    });
+    if (analyzedData) {
+        res.render('ta', {
+            data: analyzedData.taMap[req.params.taName]
+        });
+    } else {
+        updateData(function(err) {
+            if (err) {
+                res.render('ta', {
+                    data: null
+                });
+            } else {
+                res.render('ta', {
+                    data: analyzedData.taMap[req.params.taName]
+                });
+            }
+        });
+    }
 });
 
 app.get('/writeToFile', function(req, res) {
-    analyzedData.writeDataToFile();
+    if (analyzedData) {
+        analyzedData.writeDataToFile();
+    }
     res.redirect('/');
 });
