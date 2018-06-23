f97d464747231de66b9076a3444f6f13e3c590c3
Humad <humad@live.com>
2018-06-22 20:01:53 -0700

    switched from callbacks to promises

---
+++ Nodejs/ClassroomDataAnalysis/dataRetrieval.js
@@ -0,0 +1,116 @@
+const moment = require('moment-timezone');
+// Postgres setup
+const Pool = require('pg').Pool;
+
+var config = {
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    ssl: true
+}
+
+const pool = new Pool(config);
+
+let dailyData;
+let lastChecked;
+let dataForDays = {};
+
+getDailyData();
+
+function getDailyData() {
+
+    return new Promise(function(resolve, reject) {
+        if (lastChecked && ((new Date()) - lastChecked) <= 300000) {
+            lastChecked = new Date();
+            return resolve(dailyData);
+        }
+
+        if (!lastChecked) {
+            lastChecked = new Date();
+        }
+    
+        let todayString = getDate(0);
+        let tomorrowString = getDate(1);
+        let queryString = "SELECT * FROM events WHERE ts >= '" + todayString + " 16:00:00' AND ts <= '" + tomorrowString + " 05:00:00' ORDER BY ts ASC";
+
+        pool.query(queryString, function(err, result) {
+            if (err) {
+                return reject(err);
+            } else {
+                let jsonData = JSON.parse(JSON.stringify(result.rows));
+                dailyData = require('./dataAnalysis')(jsonData);
+                return resolve(dailyData);
+            }
+        });
+    });
+}
+
+function getDataForDay(dayString) {
+
+    return new Promise(function(resolve, reject) {
+        let tomorrowString = moment(dayString).add(1, 'days').tz('America/Los_Angeles').format('YYYY-MM-DD');
+        let queryString = "SELECT * FROM events WHERE ts >= '" + dayString + " 16:00:00' AND ts <= '" + tomorrowString  + " 05:00:00' ORDER BY ts ASC";
+
+        if (dataForDays.hasOwnProperty(dayString)) {
+            // If already have data for given day, no need to process again
+            resolve(dataForDays[dayString]);
+        } else {
+            pool.query(queryString, function(err, result) {
+                if (err) {
+                    reject(err);
+                } else {
+                    let jsonData = JSON.parse(JSON.stringify(result.rows));
+                    dataForDays[dayString] = require('./dataAnalysis')(jsonData);
+                    dataForDays[dayString].date = dayString;
+                    resolve(dataForDays[dayString]);
+                }
+            });
+        }
+    });
+    
+}
+
+function getDataForAllDays() {
+
+    return new Promise(function(resolve, reject) {
+        let allDays = getPastDays();
+        let allPromises = [];
+
+        for (var i = 0; i < allDays.length; i++) {
+            let currentDay = allDays[i];
+            allPromises.push(getDataForDay(currentDay));
+        }
+
+        Promise.all(allPromises).then(function(data) {
+            resolve(data);
+        });
+    });
+}
+
+// Helper functions
+function getDate(offset) {
+    return moment().add(offset, 'days').tz("America/Los_Angeles").format('YYYY-MM-DD');
+}
+
+function getPastDays() {
+    let today = moment().tz('America/Los_Angeles');
+    let pastDay = moment('2018-05-29').tz('America/Los_Angeles'); // First day of summer classes
+
+    let allDays = [];
+
+    while (pastDay < today) {
+        allDays.push(pastDay.format('YYYY-MM-DD'));
+        pastDay.add(1, 'days');
+    }
+
+    return allDays;
+}
+
+module.exports = {
+    getDailyData: getDailyData,
+    getDataForDay: getDataForDay,
+    getDataForAllDays: getDataForAllDays,
+    getDate: getDate
+}
--- Nodejs/ClassroomDataAnalysis/index.js
+++ Nodejs/ClassroomDataAnalysis/index.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const path = require('path');
 const app = express();
-var moment = require('moment-timezone');
 
 app.set("port", (process.env.PORT || 8000));
 app.set('views', path.join(__dirname, 'views'));
@@ -17,111 +16,89 @@ app.listen(app.get("port"), function() {
 // Keep Heroku app alive
 const http = require("http");
 setInterval(function() {
-    http.get("http://motivate-bot.herokuapp.com/");
+    http.get("http://helperinteractions.herokuapp.com/");
 }, 300000); // 5 Minutes 
 
 // ---- //
-
-// Postgres setup
-const Pool = require('pg').Pool;
-
-var config = {
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    ssl: true
-}
-
-const pool = new Pool(config);
-
 
 // SELECT * FROM events WHERE ts >= '2018-06-19 16:00:00' AND ts <= '2018-06-20 05:00:00' ORDER BY ts ASC;
 // Time in Postgres is 7 hours ahead Pacific Time
 // 09:00 in Pacific Time is 16:00 in Postgres time
 // So for a normal Horizons day, we need data from (today, 16:00) to (tomorrow, 05:00)
 
-let analyzedData;
-
-function getDate(offset) {
-    return moment().add(offset, 'days').tz("America/Los_Angeles").format();
-}
-
-function updateData(callback) {
-    var todayString = getDate(0).substring(0, 10);
-    var tomorrowString = getDate(1).substring(0, 10);
-    let queryString = "SELECT * FROM events WHERE ts >= '" + todayString + " 16:00:00' AND ts <= '" + tomorrowString + " 05:00:00' ORDER BY ts ASC";
-
-    pool.query(queryString, function(err, result) {
-        if (err) {
-            callback(err);
-        } else {
-            jsonData = JSON.parse(JSON.stringify(result.rows));
-            analyzedData = require('./dataAnalysis')(jsonData);
-            callback(null);
-        }
-    });
-}
+const poolParty = require('./dataRetrieval');
 
 app.get('/', function(req, res) {
-    var todayString = getDate(0).substring(0, 10);
-    updateData(function(err) {
-        if (err) {
-            res.render('index', {
-                today: todayString,
-                data: [],
-                averageWaitTime: -1,
-                averageHelpTime: -1
-            });
-        } else {
-            res.render('index', {
-                today: todayString,
-                data: analyzedData.refactoredData,
-                averageWaitTime: analyzedData.averageWaitTime,
-                averageHelpTime: analyzedData.averageHelpTime
-            });
-        }
-    });
+    var todayString = poolParty.getDate(0);
+
+    poolParty.getDailyData()
+    .then(function(data) {
+        res.render('index', {
+            today: todayString,
+            data: data.refactoredData,
+            averageWaitTime: data.averageWaitTime,
+            averageHelpTime: data.averageHelpTime
+        });
+    })
+    .catch(function(err) {
+        res.render('index', {
+            today: todayString,
+            data: [],
+            averageWaitTime: -1,
+            averageHelpTime: -1
+        });
+    })
 });
 
 app.get('/student/:studentName', function(req, res) {
-    if (analyzedData) {
+    poolParty.getDailyData()
+    .then(function(data) {
         res.render('student', {
-            data: analyzedData.studentMap[req.params.studentName]
+            data: data.studentMap[req.params.studentName]
         });
-    } else {
-        updateData(function(err) {
-            if (err) {
-                res.render('student', {
-                    data: null
-                });
-            } else {
-                res.render('student', {
-                    data: analyzedData.studentMap[req.params.studentName]
-                });
-            }
+    })
+    .catch(function(err) {
+        res.render('student', {
+            data: null
         });
-    }
+    })
 });
 
 app.get('/ta/:taName', function(req, res) {
-    if (analyzedData) {
+    poolParty.getDailyData()
+    .then(function(data) {
         res.render('ta', {
-            data: analyzedData.taMap[req.params.taName]
+            data: data.taMap[req.params.taName]
         });
+    })
+    .catch(function(err) {
+        res.render('ta', {
+            data: null
+        });
+    })
+});
+
+app.get('/days', function(req, res) {
+    if (req.query.date) {
+        poolParty.getDataForDay(req.query.date)
+        .then(function(data) {
+            res.render('day', {
+                data: data
+            });
+        })
+        .catch(function(err) {
+            res.send(err);
+        })
     } else {
-        updateData(function(err) {
-            if (err) {
-                res.render('ta', {
-                    data: null
-                });
-            } else {
-                res.render('ta', {
-                    data: analyzedData.taMap[req.params.taName]
-                });
-            }
-        });
+        poolParty.getDataForAllDays()
+        .then(function(data) {
+            res.render('allDays', {
+                data: data
+            });
+        })
+        .catch(function(err) {
+            res.send(err);
+        })
     }
 });
 
