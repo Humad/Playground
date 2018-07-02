b8e54ac4e6eab1164ec170820a5dbb0a22ea0643
Humad <humad@live.com>
2018-07-02 15:54:03 -0700

    added basic visual functionality

--- React/react-todo/reactApp/app.js
+++ React/react-todo/reactApp/app.js
@@ -1,5 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React is here!</p>,
+var dummyData = ["Come to SF", "Master React", "Sleep"];
+
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<li><button>X</button>{this.props.task}</li>);
+    }
+}
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+        <ul>
+            {dummyData.map((item) => {return <Todo task={item} />})}
+        </ul>
+        );
+    }
+}
+
+ReactDOM.render(<TodoList />,
     document.getElementById('root'));
